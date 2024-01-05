3261858da6071e9e104889c26278e739f1bba3ab
Anton Lydell <37381421+antonlydell@users.noreply.github.com>
2024-01-05 21:23:59 +0100

    Release of version 0.1.0
    Released version 0.1.0 and added CHANGELOG.md.

---
+++ src/streamlit_passwordless/__init__.py
@@ -0,0 +1,17 @@
+r"""The streamlit-passwordless package."""
+
+# Standard library
+from datetime import date
+
+
+__versiontuple__ = (0, 1, 0)
+r"""The version of streamlit-passwordless in a comparable form.
+Adheres to `Semantic Versioning <https://semver.org/spec/v2.0.0.html>`_
+(MAJOR.MINOR.PATCH).
+"""
+
+__version__ = '0.1.0'
+r"""The streamlit-passwordless version string."""
+
+__releasedate__ = date(2024, 1, 5)
+r"""The release date of the version specified in :data:`__versiontuple__`."""
