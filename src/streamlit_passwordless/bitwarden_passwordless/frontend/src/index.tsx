425b4c9da73b019d31256b4302618b4ec47bdc3f
Anton Lydell <37381421+antonlydell@users.noreply.github.com>
2024-01-23 20:55:41 +0100

    Implemented first draft of frontend register JavaScript function
    The function uses the Bitwarden Passwordless frontend client to
    register a passkey with the user's device.

--- src/streamlit_passwordless/bitwarden_passwordless/frontend/src/index.tsx
+++ src/streamlit_passwordless/bitwarden_passwordless/frontend/src/index.tsx
@@ -1,15 +1,48 @@
-import { Streamlit, RenderData } from "streamlit-component-lib"
+import { Streamlit, RenderData } from "streamlit-component-lib";
+import { Client } from '@passwordlessdev/passwordless-client';
 
-let previousOutput = ''
+let passwordlessClient: Client;
+let clientInitialized: boolean = false;
+let previousToken: string = '';
 
+/**
+ * Initialize an instance of the Bitwarden Passwordless frontend client.
+ *
+ * @param {string} apiKey - The public key of the Bitwarden Passwordless application.
+ *
+ * @returns {Client} - The Bitwarden Passwordless frontend client.
+ */
+function createPasswordlessClient(apiKey: string): Client {
+
+  if (clientInitialized === false) {
+    passwordlessClient = new Client({apiKey: apiKey});
+    clientInitialized = true
+
+    return passwordlessClient
+  } else {
+    return passwordlessClient
+  }
+
+}
+
+/**
+ * The render function that gets called every time the Streamlit Python component gets called.
+ *
+ * @param {Event} event - The input parameters from the Streamlit Python application.
+ */
 function onRender(event: Event): void {
   // Get the RenderData from the event
   const data = (event as CustomEvent<RenderData>).detail;
-  const registerToken = data.args['token'];
+  const apiKey: string = data.args['public_key'];
+  const registerToken: string = data.args['register_token'];
+  const credentialNickname: string = data.args['credential_nickname'];
 
-  if (previousOutput !== registerToken) {
-    previousOutput = registerToken;
-    Streamlit.setComponentValue([registerToken, true]);
+  const client = createPasswordlessClient(apiKey)
+  const {token, error} = client.register(registerToken, credentialNickname)
+
+  if (previousToken !== registerToken) {
+    previousToken = registerToken;
+    Streamlit.setComponentValue([token, error]);
   }
 
 }
