4622074957b6cd3eede00abc93c61c1d4ac0f70c
Anton Lydell <37381421+antonlydell@users.noreply.github.com>
2024-05-07 20:56:42 +0200

    First re-designed stage of bitwarden_register_from
    Removed the Streamlit form and replaced the form submit button
    with the `streamlit_passwordless.register_button` to trigger the
    register process. The helper function `_validate_form` has been
    replaced by the functions `_validate_username` and `_create_user`.
    A solution to handle if the register_button was clicked or not
    is needed.

--- src/streamlit_passwordless/components/ids.py
+++ src/streamlit_passwordless/components/ids.py
@@ -8,6 +8,7 @@ BP_REGISTER_FORM = 'bp-register-form'
 BP_REGISTER_FORM_USERNAME_TEXT_INPUT = 'bp-register-form-username-text-input'
 BP_REGISTER_FORM_DISPLAYNAME_TEXT_INPUT = 'bp-register-form-displayname-text-input'
 BP_REGISTER_FORM_ALIASES_TEXT_INPUT = 'bp-register-form-aliases-text-input'
+BP_REGISTER_FORM_SUBMIT_BUTTON = 'bp-register-form-submit-button'
 
 # =====================================================================================
 # Sign in form
