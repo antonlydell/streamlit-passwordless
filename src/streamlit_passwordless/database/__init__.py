c98cad71d0a3d0dffc2b70f4482e551f3967677f
Anton Lydell <37381421+antonlydell@users.noreply.github.com>
2024-06-02 11:39:16 +0200

    Updated top level imports on database sub-package
    Importing all objects that will be available in the public API.
    The imports will look like:
    
    import streamlit_passwordless as stp
    
    - stp.db : All database objects except for the models.
    
    - stp.db.models :
        All database models. The models are in its own module to not
        confuse them with the schemas that are used for creating the models.

--- src/streamlit_passwordless/database/__init__.py
+++ src/streamlit_passwordless/database/__init__.py
@@ -1,1 +1,7 @@
 r"""The database functionality of streamlit-passwordless."""
+
+from . import models
+from .cache import create_session_factory
+from .core import Session
+from .crud.user import create_user, get_all_users, get_user_by_username
+from .schemas.user import UserCreate
