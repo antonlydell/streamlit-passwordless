51f930d7a0b19bb50605d78e955dddac8433663b
Anton Lydell <37381421+antonlydell@users.noreply.github.com>
2024-06-02 15:30:41 +0200

    Added session state keys
    SK_USERNAME_IS_VALID : bool, default False
        True if the input username from the user is valid, which allows the user
        to register a passkey credential. Assumed False until proven otherwise.
    
    SK_DB_USER : streamlit_passwordless.db.models.User or None, default None
        The database user. If None the user does not exist in the database.
    
    Added prefix "stp" to all session state keys to show that they originate
    from streamlit-passwordless.

--- src/streamlit_passwordless/components/config.py
+++ src/streamlit_passwordless/components/config.py
@@ -12,7 +12,9 @@ import streamlit as st
 # Session state keys
 # =====================================================================================
 
-SK_BP_VERIFIED_USER = 'bp-verified-user'
+SK_USERNAME_IS_VALID = 'stp-username-is-valid'
+SK_BP_VERIFIED_USER = 'stp-bp-verified-user'
+SK_DB_USER = 'stp-db-user'
 
 
 # =====================================================================================
@@ -34,8 +36,19 @@ def init_session_state() -> None:
 
     Session state keys
     ------------------
-    SK_BP_VERIFIED_USER : streamlit_passwordless.BitwardenPasswordlessVerifiedUser | None, default None
+    SK_USERNAME_IS_VALID : bool, default False
+        True if the input username from the user is valid, which allows the user
+        to register a passkey credential. Assumed False until proven otherwise.
+
+    SK_BP_VERIFIED_USER : streamlit_passwordless.BitwardenPasswordlessVerifiedUser or None, default None
         A verified user from Bitwarden Passwordless.
+
+    SK_DB_USER : streamlit_passwordless.db.models.User or None, default None
+        The database user. If None the user does not exist in the database.
     """
 
-    st.session_state[SK_BP_VERIFIED_USER] = None
+    none_keys = (SK_BP_VERIFIED_USER, SK_DB_USER)
+    for key in none_keys:
+        st.session_state[key] = None
+
+    st.session_state[SK_USERNAME_IS_VALID] = False
