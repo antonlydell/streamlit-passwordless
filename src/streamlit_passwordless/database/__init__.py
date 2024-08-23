50573540df0ececd4a8d65c7c1bcf92f392ecc70
Anton Lydell <37381421+antonlydell@users.noreply.github.com>
2024-08-23 21:01:36 +0200

    Added SessionFactory to import from database sub-package
    The `SessionFactory` could be useful to access from
    `streamlit_passwordless.db` for type annotations to functions.

--- src/streamlit_passwordless/database/__init__.py
+++ src/streamlit_passwordless/database/__init__.py
@@ -2,6 +2,6 @@ r"""The database functionality of streamlit-passwordless."""
 
 from . import models
 from .cache import create_session_factory
-from .core import Session
+from .core import Session, SessionFactory
 from .crud.user import create_user, get_all_users, get_user_by_username
 from .schemas.user import UserCreate
