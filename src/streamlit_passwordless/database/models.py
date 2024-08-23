a4802cba19c5d0312b5411159e631db77e56de93
Anton Lydell <37381421+antonlydell@users.noreply.github.com>
2024-08-23 20:49:55 +0200

    Added user_sign_in table to the database
    The table keeps track of when a user has signed in to the app.

--- src/streamlit_passwordless/database/models.py
+++ src/streamlit_passwordless/database/models.py
@@ -66,6 +66,9 @@ class User(Base):
 
     emails : list[Email]
         The email addresses associated with the user.
+
+    sign_ins : list[UserSignIn]
+        Info about when the user has signed in to the application.
     """
 
     __tablename__ = 'stp_user'
@@ -77,6 +80,7 @@ class User(Base):
     disabled: Mapped[bool] = mapped_column(default=False)
     disabled_timestamp: Mapped[Optional[datetime]] = mapped_column(TIMESTAMP())
     emails: Mapped[list['Email']] = relationship(back_populates='user')
+    sign_ins: Mapped[list['UserSignIn']] = relationship(back_populates='user')
 
     def __repr__(self) -> str:
         return (
@@ -137,3 +141,82 @@ class Email(Base):
 
 Index(f'{Email.__tablename__}_email_ix', Email.email)
 Index(f'{Email.__tablename__}_user_id_ix', Email.user_id)
+
+
+class UserSignIn(Base):
+    r"""A logging table of when a user has signed in to the application.
+
+    Parameters
+    ----------
+    user_sign_in_id : int
+        The primary key of the table.
+
+    user_id : str
+        The unique ID of the user that signed in to the application.
+
+    sign_in_timestamp : datetime
+        The timestamp in timezone UTC when the user signed in.
+
+    success : bool
+        True if the user was successfully signed in and False otherwise.
+
+    origin : str
+        The domain name or IP-address of the application the user signed in to.
+
+    device : str
+        The device the user signed in from. E.g. a web browser.
+
+    country : str
+        The country code of the country that the user signed in from. E.g. SE for Sweden.
+
+    credential_nickname : str
+        The nickname of the passkey that was used to sign in.
+
+    credential_id : str
+        The ID of the passkey credential used to sign in.
+
+    sign_in_type : str
+        The type of sign in method. E.g. 'passkey_signin'.
+
+    rp_id : str or None
+        The ID of the relaying party, which is the server that
+        verifies the credentials during the sign in process.
+
+    user : User
+        The user object the sign in entry belongs to.
+    """
+
+    __tablename__ = 'stp_user_sign_in'
+
+    user_sign_in_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    user_id: Mapped[str] = mapped_column(ForeignKey(User.user_id))
+    sign_in_timestamp: Mapped[datetime] = mapped_column(TIMESTAMP())
+    success: Mapped[bool]
+    origin: Mapped[str]
+    device: Mapped[str]
+    country: Mapped[str]
+    credential_nickname: Mapped[str]
+    credential_id: Mapped[str]
+    sign_in_type: Mapped[str]
+    rp_id: Mapped[Optional[str]]
+    user: Mapped['User'] = relationship(back_populates='sign_ins')
+
+    def __repr__(self) -> str:
+        return (
+            f'{self.__class__.__name__}(\n'
+            f'    user_sign_in_id={self.user_sign_in_id},\n'
+            f'    user_id={self.user_id},\n'
+            f'    sign_in_timestamp={self.sign_in_timestamp},\n'
+            f'    success={self.success},\n'
+            f'    origin={self.origin},\n'
+            f'    device={self.device},\n'
+            f'    country={self.country},\n'
+            f'    credential_nickname={self.credential_nickname},\n'
+            f'    credential_id={self.credential_id},\n'
+            f'    sign_in_type={self.sign_in_type},\n'
+            f'    rp_id={self.rp_id},\n)'
+        )
+
+
+Index(f'{UserSignIn.__tablename__}_ix1', UserSignIn.user_id, UserSignIn.sign_in_timestamp)
+Index(f'{UserSignIn.__tablename__}_ix2', UserSignIn.origin)
