6a15c363c5d2f767c67502d016fef34df71eaa4f
Anton Lydell <37381421+antonlydell@users.noreply.github.com>
2024-09-27 21:19:54 +0200

    Added and updated unit tests for the new models
    The new models (`models.Email`, `models.UserSignIn`)
    will be analogous to the database models. `models.User`
    should be updated to reflect its database model.

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,6 +15,9 @@ from pydantic import AnyHttpUrl
 import streamlit_passwordless.bitwarden_passwordless.backend
 from streamlit_passwordless import common, models
 from streamlit_passwordless.bitwarden_passwordless.backend import BitwardenPasswordlessVerifiedUser
+from streamlit_passwordless.database import models as db_models
+
+from .config import TZ_UTC, ModelData
 
 # =============================================================================================
 # Models
@@ -22,14 +25,14 @@ from streamlit_passwordless.bitwarden_passwordless.backend import BitwardenPassw
 
 
 @pytest.fixture(scope='session')
-def user_id() -> str:
-    r"""The user ID to use for the test user of the test suite."""
+def user_1_user_id() -> str:
+    r"""The user ID for test user 1 of the test suite."""
 
-    return 'mocked-user_id'
+    return 'user_1_user_id'
 
 
 @pytest.fixture()
-def mocked_user_id(user_id: str, monkeypatch: pytest.MonkeyPatch) -> str:
+def mocked_user_id(user_1_user_id: str, monkeypatch: pytest.MonkeyPatch) -> str:
     r"""Mock the user ID that is generated if a user ID is not supplied to the `User` model.
 
     Returns
@@ -38,27 +41,263 @@ def mocked_user_id(user_id: str, monkeypatch: pytest.MonkeyPatch) -> str:
         The user_id that is returned from the mock.
     """
 
-    monkeypatch.setattr(models.uuid, 'uuid4', Mock(return_value=user_id))
+    monkeypatch.setattr(models.uuid, 'uuid4', Mock(return_value=user_1_user_id))
 
-    return user_id
+    return user_1_user_id
 
 
 @pytest.fixture(scope='session')
-def user(user_id: str) -> models.User:
-    r"""A test user to use for the test suite.
+def user_1_email_primary(user_1_user_id: str) -> tuple[models.Email, db_models.Email, ModelData]:
+    r"""The primary email of test user 1.
 
     Returns
     -------
-    streamlit_passwordless.models.User
+    model : streamlit_passwordless.models.Email
+        The model of the email address.
+
+    db_model : streamlit_passwordless.database.models.Email
+        The database model of the email address.
+
+    data : ModelData
+        The input data that created `model`.
     """
 
-    return models.User(
-        username='m.shadows',
+    data = {
+        'email_id': 1,
+        'user_id': user_1_user_id,
+        'email': 'rev@ax7.com',
+        'is_primary': True,
+        'verified_at': datetime(1999, 1, 1, 1, 1, 1, tzinfo=TZ_UTC),
+        'disabled': False,
+        'disabled_timestamp': None,
+    }
+
+    model = models.Email.model_validate(data)
+    db_model = db_models.Email(**data)
+
+    return model, db_model, data
+
+
+@pytest.fixture(scope='session')
+def user_1_email_secondary(user_1_user_id: str) -> tuple[models.Email, db_models.Email, ModelData]:
+    r"""The secondary email of test user 1.
+
+    Returns
+    -------
+    model : streamlit_passwordless.models.Email
+        The model of the email address.
+
+    db_model : streamlit_passwordless.database.models.Email
+        The database model of the email address.
+
+    data : ModelData
+        The input data that created `model`.
+    """
+
+    data = {
+        'email_id': 2,
+        'user_id': user_1_user_id,
+        'email': 'the.rev@ax7.com',
+        'is_primary': False,
+        'verified_at': None,
+        'disabled': True,
+        'disabled_timestamp': datetime(2007, 10, 30, 12, 0, 0, tzinfo=TZ_UTC),
+    }
+
+    model = models.Email.model_validate(data)
+    db_model = db_models.Email(**data)
+
+    return model, db_model, data
+
+
+@pytest.fixture(scope='session')
+def user_1_sign_in_successful(
+    user_1_user_id: str,
+) -> tuple[models.UserSignIn, db_models.UserSignIn, ModelData]:
+    r"""A successful passkey sign in for test user 1.
+
+    Returns
+    -------
+    model : streamlit_passwordless.models.UserSignIn
+        The model of the user sign in.
+
+    db_model : streamlit_passwordless.database.models.UserSignIn
+        The database model of the user sign in.
+
+    data : ModelData
+        The input data that created `model`.
+    """
+
+    data = {
+        'user_sign_in_id': 1,
+        'user_id': user_1_user_id,
+        'sign_in_timestamp': datetime(2005, 6, 6, 13, 37, 37, tzinfo=TZ_UTC),
+        'success': True,
+        'origin': 'http://localhost:8501/',
+        'device': 'Google Chrome',
+        'country': 'US',
+        'credential_nickname': 'beast_and_the_harlot',
+        'credential_id': 'credential_id_1',
+        'sign_in_type': 'passkey_signin',
+        'rp_id': None,
+    }
+
+    model = models.UserSignIn.model_validate(data)
+    db_model = db_models.UserSignIn(**data)
+
+    return model, db_model, data
+
+
+@pytest.fixture(scope='session')
+def user_1_sign_in_unsuccessful(
+    user_1_user_id: str,
+) -> tuple[models.UserSignIn, db_models.UserSignIn, ModelData]:
+    r"""An unsuccessful passkey sign in for test user 1.
+
+    Returns
+    -------
+    model : streamlit_passwordless.models.UserSignIn
+        The model of the user sign in.
+
+    db_model : streamlit_passwordless.database.models.UserSignIn
+        The database model of the user sign in.
+
+    data : ModelData
+        The input data that created `model`.
+    """
+
+    data = {
+        'user_sign_in_id': 2,
+        'user_id': user_1_user_id,
+        'sign_in_timestamp': datetime(2003, 8, 26, 13, 37, 37, tzinfo=TZ_UTC),
+        'success': False,
+        'origin': 'https://ax7.com/',
+        'device': 'Firefox',
+        'country': 'SE',
+        'credential_nickname': 'afterlife',
+        'credential_id': 'credential_id_2',
+        'sign_in_type': 'passkey_register',
+        'rp_id': 'critical_acclaim',
+    }
+
+    model = models.UserSignIn.model_validate(data)
+    db_model = db_models.UserSignIn(**data)
+
+    return model, db_model, data
+
+
+@pytest.fixture(scope='session')
+def user_1(user_1_user_id: str) -> tuple[models.User, db_models.User, ModelData]:
+    r"""Test user 1 without emails and no passkey sign in.
+
+    Returns
+    -------
+    model : streamlit_passwordless.models.User
+        The model of the user.
+
+    db_model : streamlit_passwordless.database.models.User
+        The database model of the user.
+
+    data : ModelData
+        The input data that created `model`.
+    """
+
+    user_id = user_1_user_id
+    username = 'rev'
+    ad_username = 'the.rev'
+    displayname = 'Jimmy'
+    verified_at = None
+    disabled = False
+    disabled_timestamp = None
+
+    data = {
+        'user_id': user_id,
+        'username': username,
+        'ad_username': ad_username,
+        'displayname': displayname,
+        'verified_at': verified_at,
+        'disabled': disabled,
+        'disabled_timestamp': disabled_timestamp,
+        'emails': None,
+        'sign_in': None,
+        'aliases': None,
+    }
+
+    model = models.User.model_validate(data)
+    db_model = db_models.User(
         user_id=user_id,
-        email='m.shadows@ax7.com',
-        displayname='M Shadows',
-        aliases=('Matt', 'Shadows'),
+        username=username,
+        ad_username=ad_username,
+        displayname=displayname,
+        verified_at=verified_at,
+        disabled=disabled,
+        disabled_timestamp=disabled_timestamp,
     )
+
+    return model, db_model, data
+
+
+@pytest.fixture(scope='session')
+def user_1_with_2_emails_and_successful_signin(
+    user_1: tuple[models.User, db_models.User, ModelData],
+    user_1_email_primary: tuple[models.Email, db_models.Email, ModelData],
+    user_1_email_secondary: tuple[models.Email, db_models.Email, ModelData],
+    user_1_sign_in_successful: tuple[models.UserSignIn, db_models.UserSignIn, ModelData],
+) -> tuple[models.User, db_models.User, ModelData]:
+    r"""Test user 1 with two emails and a successful passkey sign in.
+
+    Returns
+    -------
+    model : streamlit_passwordless.models.User
+        The model of the user.
+
+    db_model : streamlit_passwordless.database.models.User
+        The database model of the user.
+
+    data : ModelData
+        The input data that created `model`.
+    """
+
+    email_primary, _, _ = user_1_email_primary
+    email_secondary, _, _ = user_1_email_secondary
+    _, _, sign_in_data = user_1_sign_in_successful
+    _, db_model, user_data = user_1
+
+    data = user_data.copy()
+    data['emails'] = [email_primary, email_secondary]
+    data['sign_in'] = sign_in_data
+    model = models.User.model_validate(data)
+
+    return model, db_model, data
+
+
+@pytest.fixture(scope='session')
+def user_1_with_unsuccessful_signin(
+    user_1: tuple[models.User, db_models.User, ModelData],
+    user_1_sign_in_unsuccessful: tuple[models.UserSignIn, db_models.UserSignIn, ModelData],
+) -> tuple[models.User, db_models.User, ModelData]:
+    r"""Test user 1 with no emails and an unsuccessful passkey sign in.
+
+    Returns
+    -------
+    model : streamlit_passwordless.models.User
+        The model of the user.
+
+    db_model : streamlit_passwordless.database.models.User
+        The database model of the user.
+
+    data : ModelData
+        The input data that created `model`.
+    """
+
+    _, _, sign_in_data = user_1_sign_in_unsuccessful
+    _, db_model, user_data = user_1
+
+    data = user_data.copy()
+    data['sign_in'] = sign_in_data
+    model = models.User.model_validate(data)
+
+    return model, db_model, data
 
 
 @pytest.fixture()
