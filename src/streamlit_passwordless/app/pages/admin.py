606965e6254303a3b5a04afe6db61872d54245cb
Anton Lydell <37381421+antonlydell@users.noreply.github.com>
2025-01-11 18:20:53 +0100

    Added authorization to app.pages.admin
    Added func `admin_page_authorized`, which is executed when the page module
    is run as script or Streamlit page. It is defined as `admin_page` decorated
    with `authorization.authorized` to only allows admin users to access its
    contents. If not authorized the user is redirected to the sign in page.
    `admin_page_authorized` is used by the CLI command `stp run admin`. The
    func `admin_page` is unchanged to let the user of `streamlit_passwordless`
    use `admin_page` in his application and apply the `authorization.authorized`
    decorator as desired.

--- src/streamlit_passwordless/app/pages/admin.py
+++ src/streamlit_passwordless/app/pages/admin.py
@@ -8,9 +8,12 @@ from streamlit_passwordless.app.config import (
     APP_HOME_PAGE_URL,
     APP_ISSUES_PAGE_URL,
     MAINTAINER_INFO,
+    Pages,
     setup,
 )
 from streamlit_passwordless.app.controllers.admin import controller
+from streamlit_passwordless.authorization import authorized
+from streamlit_passwordless.models import AdminRole
 
 ABOUT = f"""\
 The Streamlit Passwordless admin web app.
@@ -40,5 +43,7 @@ def admin_page() -> None:
         controller(client=client, db_session=session)
 
 
+admin_page_authorized = authorized(redirect=Pages.SIGN_IN, role=AdminRole)(admin_page)
+
 if __name__ == '__main__' or __name__ == '__page__':
-    admin_page()
+    admin_page_authorized()
