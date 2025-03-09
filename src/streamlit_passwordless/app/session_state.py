7aa8b980f198d2e9b66da0dd92f3148cdb619a39
Anton Lydell <37381421+antonlydell@users.noreply.github.com>
2025-03-09 14:13:03 +0100

    Added module app.session_state
    Functionality for managing the session state of the web apps.
    Added the session state keys needed by the admin app.

---
+++ src/streamlit_passwordless/app/session_state.py
@@ -0,0 +1,9 @@
+r"""Functionality for managing the session state of the web apps."""
+
+# =====================================================================================
+# Session state keys
+# =====================================================================================
+
+SK_DB_ROLES = 'stp-db-roles'
+SK_DB_CUSTOM_ROLES = 'stp-db-custom-roles'
+SK_SELECTABLE_USERS_DATAFRAME = 'stp-selectable-users-dataframe'
