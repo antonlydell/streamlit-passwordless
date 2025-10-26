46725d8241091eaa4e62002355f58bff5b82c810
Anton Lydell <37381421+antonlydell@users.noreply.github.com>
2025-10-26 20:30:57 +0100

    Updated example application
    Renamed examples/app.py to examples/mvp.py and updated the example
    to reflect the modern way of using streamlit-passwordless.

---
+++ examples/mvp.py
@@ -0,0 +1,28 @@
+r"""A minimal single-page Streamlit application demonstrating basic passkey registration and sign-in."""
+
+# Third party
+import streamlit as st
+
+# Local
+import streamlit_passwordless as stp
+
+
+def main() -> None:
+    st.set_page_config(page_title='Streamlit Passwordless', page_icon='✨')
+    st.title('Streamlit Passwordless Demo')
+
+    # initialize client and database
+    client, session_factory, _ = stp.setup(create_database=True)
+    with session_factory() as session:
+        stp.db.init(_session=session)  # create the default roles once on startup
+
+        with st.container(border=True):
+            stp.bitwarden_register_form(client=client, db_session=session, border=False)
+            st.write('Already have an account?')
+            stp.bitwarden_sign_in_button(client=client, db_session=session)
+
+    stp.sign_out_button(use_container_width=True)
+
+
+if __name__ == '__main__':
+    main()
